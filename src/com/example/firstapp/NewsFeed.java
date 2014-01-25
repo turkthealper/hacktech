91d34e4b4174d57660ff6217b7cb1ff26117265c
Alper Vural <alpervural@berkeley.edu>
2014-01-25 06:24:47 -0800

    made listview

--- src/com/example/firstapp/NewsFeed.java
+++ src/com/example/firstapp/NewsFeed.java
@@ -1,8 +1,16 @@
 package com.example.firstapp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class NewsFeed extends Activity {
 
@@ -10,7 +18,23 @@ public class NewsFeed extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_news_feed);
+		List<Map<String, String>> commentList = new ArrayList<Map<String,String>>();
+        commentList.add(createComment("k", "AAA"));
+        commentList.add(createComment("k", "BBB"));
+        commentList.add(createComment("k", "CCC"));
+        commentList.add(createComment("k", "DDD"));
+        ListView lv = (ListView) findViewById(R.id.list1);
+        SimpleAdapter simpleAdpt = new SimpleAdapter(this, commentList, android.R.layout.simple_list_item_1, 
+        new String[] {"k"}, new int[] {android.R.id.text1});
+        lv.setAdapter(simpleAdpt);
 	}
+	
+	private HashMap<String, String> createComment(String key, String name) {
+		    HashMap<String, String> comment = new HashMap<String, String>();
+		    comment.put(key, name);
+		    return comment;
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
