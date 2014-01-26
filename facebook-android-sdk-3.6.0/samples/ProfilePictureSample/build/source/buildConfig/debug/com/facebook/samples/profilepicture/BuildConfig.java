c52ba6abaaead5b317e53b2a170e8e043e1e2231
prithvirajpandian <prithvi.pandian@berkeley.edu>
2014-01-25 17:27:46 -0800

    added sdk

---
+++ facebook-android-sdk-3.6.0/samples/ProfilePictureSample/build/source/buildConfig/debug/com/facebook/samples/profilepicture/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.facebook.samples.profilepicture;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
